a19c6f3f2d40035f668dc38177dc11ec55d979f1
Gabriel Antonio <toni.gamv@gmail.com>
2025-08-03 18:31:32 -0300

    Atualização - Organizando Layout

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -1,25 +1,13 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const botoes = document.querySelectorAll(".header-buttons button");
-
-//     botoes.forEach(botao => {
-//         botao.addEventListener("click", function () {
-//             botoes.forEach(btn => btn.classList.remove("active"));
-//             this.classList.add("active");
-//         });
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
     const repoList = document.getElementById("repo-list");
 
-    fetch("https://api.github.com/users/toniamv/repos")
-        .then(response => response.json())
-        .then(data => {
-            repoList.innerHTML = ""; // Clear previous content
+    fetch("https://api.github.com/users/toniamv/repos").then(response => response.json()).then(data => {
+            repoList.innerHTML = "";
             data.forEach(repo => {
                 const listItem = document.createElement("li");
                 if(repo.name != "toniamv"){
-                    listItem.innerHTML = `<p>${repo.name}</p>`;
+                    listItem.innerHTML = `<p class="p_titulo">${repo.name}</p>
+                                        <p class = "p_descricao"> ${repo.description} </p>`;
                     repoList.appendChild(listItem);
                 }
             });
